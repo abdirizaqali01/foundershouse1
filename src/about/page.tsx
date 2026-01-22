0a45f18a3673697ee6346adabe7aafb2c8ed5528
amplital-jason <jason@amplital.com>
2026-01-22 07:57:08 +0200

    Join page completion. About page updates to optimize performance: GridDistortion commented out, replaced with normal img element.

--- src/about/page.tsx
+++ src/about/page.tsx
@@ -289,11 +289,11 @@ export default function AboutPage() {
                 </ParallaxMotion>
               </motion.div>        
               
-                <motion.div className="section-2-content" style={{ y: section2Content }}>
-                  <ParallaxMotion speedX={30} speedY={32} easing={[0.17, 0.67, 0.3, 0.99]}>
-                    <p>Founders House Helsinki is the hub for Finland’s most promising next-gen startups. A home for builders who move faster than anyone else, think differently, and raise the bar for everyone around them.</p>
-                  </ParallaxMotion>
-                </motion.div>
+              <motion.div className="section-2-content" style={{ y: section2Content }}>
+                <ParallaxMotion speedX={30} speedY={32} easing={[0.17, 0.67, 0.3, 0.99]}>
+                  <p>Founders House Helsinki is the hub for Finland’s most promising next-gen startups. A home for builders who move faster than anyone else, think differently, and raise the bar for everyone around them.</p>
+                </ParallaxMotion>
+              </motion.div>
             </div>
 
             <motion.div className="section-3" style={{ y: section3 }} ref={section3Ref}>
@@ -386,9 +386,10 @@ export default function AboutPage() {
               <div className="team-img-wrapper">
                 <ParallaxMotion speedX={15} speedY={15} easing={[0.17, 0.67, 0.3, 0.99]}>
                   <div className="team-img-container">
-                    {/*
-                    <img src={FOUNDERS_HOUSE_TEAM_IMG_SRC} alt="" />
-                    */}
+                    
+                    <img src={FOUNDERS_HOUSE_TEAM_IMG_SRC} alt="Founders House Team" />
+                    
+                    {/*}
                     <GridDistortion
                       imageSrc={FOUNDERS_HOUSE_TEAM_IMG_SRC}
                       grid={20}
@@ -397,6 +398,7 @@ export default function AboutPage() {
                       relaxation={0.95}
                       className="team-distortion-img"
                     />
+                    */}
                   </div>
                 </ParallaxMotion>
               </div>
